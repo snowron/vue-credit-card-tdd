a8313d83ab1fe92b889f391bfe4763864b18d4d0
Murat <snowronark@gmail.com>
2021-02-10 14:08:10 +0300

    Added new tests and little css make up

--- components/CreditCard.vue
+++ components/CreditCard.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <div id="errorSide" v-if="error !== ''">
+  <div class="background">
+    <div id="errorSide" class="errorContainer" v-if="error !== ''">
       {{ error }}
     </div>
-    <div>
-      <input id="cardNumber" v-model="cardNumber" maxlength="16" />
-      <select id="cardMonth" v-model="cardMonth" required>
+    <div class="cardContainer">
+      <input
+        id="cardNumber"
+        class="cardInput"
+        placeholder="Card Number"
+        v-model="cardNumber"
+        maxlength="16"
+      />
+      <select id="cardMonth" class="cardInput" v-model="cardMonth" required>
         <option value="" disabled selected>Month</option>
         <option
           :value="n < 10 ? '0' + n : n"
@@ -16,7 +22,7 @@
           {{ n < 10 ? '0' + n : n }}
         </option>
       </select>
-      <select id="cardYear" v-model="cardYear">
+      <select id="cardYear" class="cardInput" v-model="cardYear">
         <option value="" disabled selected>Year</option>
         <option
           :value="$index + minCardYear"
@@ -26,7 +32,13 @@
           {{ $index + minCardYear }}
         </option>
       </select>
-      <input id="cardCVV" v-model="cardCVV" maxlength="3" />
+      <input
+        id="cardCVV"
+        class="cardInput"
+        placeholder="Card CVV Number"
+        v-model="cardCVV"
+        maxlength="3"
+      />
       <button id="payButton" @click="pay()">Pay with Credit Card</button>
     </div>
   </div>
@@ -49,11 +61,20 @@ export default {
     async pay() {
       this.error = ''
       if (this.cardNumber.length < 16) {
-        this.error = 'Card Number cannot be less than 16 letter'
+        this.error = 'Card Number cannot be less than 16 digits'
+        return
+      }
+      let onlyDigitsExp = new RegExp('^[0-9]+$')
+      if (!onlyDigitsExp.test(this.cardNumber)) {
+        this.error = 'Card Number can be 16 digits not letters'
         return
       }
       if (this.cardCVV.length < 3) {
-        this.error = 'Card CVV cannot be less than 3 letter'
+        this.error = 'Card CVV cannot be less than 3 digits'
+        return
+      }
+      if (!onlyDigitsExp.test(this.cardCVV)) {
+        this.error = 'Card CVV can be 3 digits not letters'
         return
       }
       if (!this.cardMonth || !this.cardYear) {
@@ -61,17 +82,61 @@ export default {
         return
       }
 
-      const ip =  this.$axios
+      const ip = this.$axios
         .$get('http://icanhazip.com')
         .then((res) => {
-         })
-        .catch((err) => {
-           this.error = 'Network error'
+          window.alert('Success')
         })
-     },
+        .catch((err) => {
+          console.log(err)
+          this.error = 'Network error'
+        })
+    },
   },
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Source+Code+Pro:400,500,600,700|Source+Sans+Pro:400,600,700&display=swap');
+.background {
+  font-family: 'Source Sans Pro', sans-serif;
+}
+.cardContainer {
+  display: flex;
+  flex-direction: column;
+}
+.cardInput {
+  margin: 20px 50px;
+}
+select {
+  height: 50px;
+  padding: 5px 15px;
+  box-shadow: none;
+  border: 1px solid #ced6e0;
+}
+input {
+  width: 80%;
+  height: 50px;
+  border-radius: 5px;
+  box-shadow: none;
+  border: 1px solid #ced6e0;
+  font-size: 18px;
+  padding: 5px 15px;
+  background: none;
+  color: #1a3b5d;
+}
+button {
+  width: 30%;
+  height: 50px;
+  border-radius: 5px;
+  margin-left: 30%;
+  color: white;
+  background-color: red;
+}
+.errorContainer {
+  padding: 1%;
+  background-color: lightcoral;
+  text-align: center;
+  color: white;
+}
 </style>
--- test/component/creditCartComponent.spec.js
+++ test/component/creditCartComponent.spec.js
@@ -1,6 +1,5 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import CreditCard from '@/components/CreditCard.vue'
-import vue from "vue";
 
 describe('Credit Cart Component Tests', () => {
   describe('Rendering Tests', () => {
@@ -39,7 +38,7 @@ describe('Credit Cart Component Tests', () => {
       expect(payButton.exists()).toBe(true)
     })
   })
-  test('Should show card number must be 16 letter error message when Pay With Credit Card button clicked', async () => {
+  test('Should show card number must be 16 digits error message when Pay With Credit Card button clicked', async () => {
     const wrapper = mount(CreditCard)
 
     const payButton = wrapper.find('#payButton')
@@ -54,9 +53,30 @@ describe('Credit Cart Component Tests', () => {
 
     await payButton.trigger('click')
     const errorMessage = wrapper.find('#errorSide')
-    expect(errorMessage.text()).toEqual("Card Number cannot be less than 16 letter")
+    expect(errorMessage.text()).toEqual("Card Number cannot be less than 16 digits")
   })
-  test('Should show cvv number must be 3 letter error message when Pay With Credit Card button clicked', async () => {
+  test('Should show card number must be 16 digits not letters error message when Pay With Credit Card button clicked', async () => {
+    const wrapper = mount(CreditCard)
+
+    const payButton = wrapper.find('#payButton')
+
+    const cardNumber = wrapper.find('#cardNumber')
+    cardNumber.setValue('12345678A9876543')
+
+    const cvv = wrapper.find('#cardCVV')
+    cvv.setValue('123')
+
+    const cardMonth = wrapper.find('#cardMonth')
+    cardMonth.setValue('01')
+
+    const cardYear = wrapper.find('#cardYear')
+    cardYear.setValue('2021')
+
+    await payButton.trigger('click')
+    const errorMessage = wrapper.find('#errorSide')
+    expect(errorMessage.text()).toEqual("Card Number can be 16 digits not letters")
+  })
+  test('Should show cvv number must be 3 digits error message when Pay With Credit Card button clicked', async () => {
     const wrapper = mount(CreditCard)
 
     const cardNumber = wrapper.find('#cardNumber')
@@ -72,7 +92,29 @@ describe('Credit Cart Component Tests', () => {
     await payButton.trigger('click')
 
     const errorMessage = wrapper.find('#errorSide')
-    expect(errorMessage.text()).toEqual("Card CVV cannot be less than 3 letter")
+    expect(errorMessage.text()).toEqual("Card CVV cannot be less than 3 digits")
+  })
+  test('Should show cvv number must be 3 digits error message when Pay With Credit Card button clicked', async () => {
+    const wrapper = mount(CreditCard)
+
+    const cardNumber = wrapper.find('#cardNumber')
+    cardNumber.setValue('1234567899876543')
+
+    const cardMonth = wrapper.find('#cardMonth')
+    cardMonth.setValue('01')
+
+    const cardYear = wrapper.find('#cardYear')
+    cardYear.setValue('2021')
+
+    const cardCVV = wrapper.find('#cardCVV')
+    cardCVV.setValue('AAA')
+
+
+    const payButton = wrapper.find('#payButton')
+    await payButton.trigger('click')
+
+    const errorMessage = wrapper.find('#errorSide')
+    expect(errorMessage.text()).toEqual("Card CVV can be 3 digits not letters")
   })
   test('Should date error when Pay With Credit Card button clicked', async () => {
 
@@ -124,11 +166,18 @@ describe('Credit Cart Component Tests', () => {
   })
   test('Should succesfully pay when Pay With Credit Card button clicked', async () => {
 
+
     const wrapper = shallowMount(CreditCard, {
       mocks: {
-        $axios: { $get: jest.fn(() => Promise.resolve()) }
+        $axios: { $get: jest.fn(() => Promise.resolve()) },
+
       }
+    
     })
+    
+    let windowSpy = jest.spyOn(window, "alert").mockImplementation((message) => {
+      console.log(message)
+    });
 
     const cardNumber = wrapper.find('#cardNumber')
     cardNumber.setValue('1234567899876543')
@@ -145,11 +194,9 @@ describe('Credit Cart Component Tests', () => {
     const payButton = wrapper.find('#payButton')
     await payButton.trigger('click')
 
-    const errorMessage = wrapper.find('#errorSide')
-    setTimeout(() => {
-      expect(errorMessage.text()).toEqual("Network error")
-      done()
-    }, 1000)
+    expect(windowSpy).toBeCalled()
+    expect(windowSpy).toBeCalledTimes(1)
+
   })
-  //
+
 })
